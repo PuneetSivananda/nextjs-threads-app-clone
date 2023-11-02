21c04741f9065c03fa49f08a804a6835762035e1
Puneet Sivananda <puneet.sivananda@walmart.com>
2023-11-02 11:14:56 +0530

    fix: layout of the application.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import { UserButton } from "@clerk/nextjs"
+// import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <main>
-      <UserButton afterSignOutUrl="/" />
+      <h1 className="head-text text-left">
+        Home
+      </h1>
     </main>
   )
 }
