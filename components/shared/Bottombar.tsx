a2828d59e4edd4c771499bcdf27676680d2e9d04
Puneet Sivananda <puneet.sivananda@gmail.com>
2023-11-04 18:47:18 +0530

    feat: makes the app into a mobile app

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,3 +1,33 @@
+"use client"
+
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image"
+import { usePathname } from "next/navigation"
+
 export default function Bottombar(){
-    return (<>Bottombar</>)
+    const pathname = usePathname()
+    return (<section className="bottombar">
+        <div className="bottombar_container">
+            {/* only visible for mobile devices */}
+
+            {sidebarLinks.map((link)=> {
+                const isActive = (pathname.includes(link.route) && link.route.length>1) || pathname == link.route
+                  return (<Link 
+                    href={link.route}
+                    key={link.label}
+                    className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+                  >
+                    <Image 
+                        src={link.imgURL}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                    />
+                    <p className="text-light-1 max-lg:hidden">{link.label}</p>
+                </Link>  
+                    )})
+                }
+        </div>
+    </section>)
 }
