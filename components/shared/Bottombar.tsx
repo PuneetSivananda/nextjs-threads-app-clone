3c8f71e2b436b9d8382fddd8fb46659015cee9e4
Puneet Sivananda <puneet.sivananda@gmail.com>
2023-11-04 18:53:03 +0530

    feat: make bottmbar with text

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -16,7 +16,7 @@ export default function Bottombar(){
                   return (<Link 
                     href={link.route}
                     key={link.label}
-                    className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+                    className={`bottombar_link ${isActive && 'bg-primary-500'}`}
                   >
                     <Image 
                         src={link.imgURL}
@@ -24,7 +24,9 @@ export default function Bottombar(){
                         width={24}
                         height={24}
                     />
-                    <p className="text-light-1 max-lg:hidden">{link.label}</p>
+                    <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                        {link.label.split(/\s+./)[0]}
+                    </p>
                 </Link>  
                     )})
                 }
