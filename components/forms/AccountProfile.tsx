c5263a0a009281ff9e9700750e7d651f41ca0872
Puneet Sivananda <puneet.sivananda@gmail.com>
2023-11-13 12:10:11 +0530

    wip: integration of user profile page

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,7 +1,21 @@
 "use client"
 
-const AccountProfile = ()=>{
-    return (<></>)
+interface Props{
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string
+}
+
+const AccountProfile = ({user, btnTitle}:Props)=>{
+    return (<div>
+        Accounr Profile
+    </div>)
 }
 
 export default AccountProfile
